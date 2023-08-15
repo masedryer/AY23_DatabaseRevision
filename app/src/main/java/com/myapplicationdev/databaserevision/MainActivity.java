9f76d70d23c53a374138e1f633c3ae571ab6b3a6
22012205 <22012205@myrp.edu.sg>
2023-08-15 12:16:52 +0800

    u

--- app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
@@ -2,6 +2,8 @@ package com.myapplicationdev.databaserevision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,28 +11,39 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnInsertRecord, btnRetrieveRecords;
+    Button btnInsertRecord, btnRetrieveRecordstv,btnRetrieveRecordslv;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnInsertRecord = findViewById(R.id.btnInsertRecord);
-        btnRetrieveRecords = findViewById(R.id.btnGetRecord);
+        btnRetrieveRecordstv = findViewById(R.id.btnGetRecordtv);
+        btnRetrieveRecordslv = findViewById(R.id.btnGetRecordlv);
 
         btnInsertRecord.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Intent intent=new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(intent);
 
             }
         });
 
-        btnRetrieveRecords.setOnClickListener(new View.OnClickListener(){
+        btnRetrieveRecordstv.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Intent intent=new Intent(MainActivity.this, RetrieveActivityTextView.class);
+                startActivity(intent);
+            }
+        });
+        btnRetrieveRecordslv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, RetrieveActivityListView.class);
+                startActivity(intent);
             }
         });
 
