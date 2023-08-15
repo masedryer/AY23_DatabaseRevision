cf2f43815c1400f9c3efbcc256e7f94aa19294ec
22023910 <22023910@myrp.edu.sg>
2023-08-15 12:19:42 +0800

    updated note.java

--- app/src/main/java/com/myapplicationdev/databaserevision/Note.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/Note.java
@@ -5,6 +5,44 @@ public class Note {
     private String content;
     private String priority;
 
-    //TODO: What should be here?
+    public Note(int id, String content, String priority) {
+        this.id = id;
+        this.content = content;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", priority='" + priority + '\'' +
+                '}';
+    }
+//TODO: What should be here?
 
 }
